6e483eff89f9da402ed06147e076e99db97827bf
Copser <iamrmot@gmail.com>
2016-04-22 22:24:23 +0200

    Delete previous jquery code, I want to build it to be fluent and to separate logic from effect

--- bower_components/bootstrap/dist/js/thrustdrilling.js
+++ bower_components/bootstrap/dist/js/thrustdrilling.js
@@ -1,36 +1,5 @@
 // Buidling Thrustdrilling animation, beginning with JQuery
 
 $(function () {
-    $('#anime1').on('click', function(e){
-        e.preventDefault();
-        $('#col7').hide('slow');
-        $('#col8').hide('slow');
-
-    });
-
-    $('#anime2').on('click', function(e){
-        e.preventDefault();
-        $('#col1').hide('slow');
-        $('#col2').hide('slow');
-        $('#col3').hide('slow');
-        $('#col4').toggleClass('col-sm-12 col-md-9');
-        $('#col5').hide('slow');
-        $('#col6').hide('slow');
-        $('#col7').hide('slow');
-        $('#col8').hide('slow');
-
-    });
-
-    $('#anime3').on('click', function(e){
-        e.preventDefault();
-        $('#col1').show('');
-        $('#col2').show('');
-        $('#col3').show('');
-        $('#col4').show('');
-        $('#col5').show('');
-        $('#col6').show('');
-        $('#col7').show('');
-        $('#col8').show('');
-    });
     
 });
