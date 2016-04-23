649315721e42c4804880c99864cd8c2970401f4f
Copser <iamrmot@gmail.com>
2016-04-23 22:57:27 +0200

    for looping animation

--- bower_components/bootstrap/dist/js/thrustdrilling.js
+++ bower_components/bootstrap/dist/js/thrustdrilling.js
@@ -1,5 +1,18 @@
 // Buidling Thrustdrilling animation, beginning with JQuery
+$(function(){
 
-$(function () {
-    
+    $('#anime1').click(function(){
+        $('.column').each(function(){
+            $( this ).hide('slow');
+        });
+        $('.columns').each(function(){
+            $( this ).show('slow');
+        });
+    });
+
+    $('#anime2').click(function(){
+        $('.columns').hide('slow');
+        $('.column').hide('slow');
+    });
+
 });
