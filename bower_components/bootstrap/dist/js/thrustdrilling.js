b39a98bebe7693b5342cf39bbf06b15651ef0189
Copser <iamrmot@gmail.com>
2016-04-24 05:35:59 +0200

    Finishing initial jquery animation

--- bower_components/bootstrap/dist/js/thrustdrilling.js
+++ bower_components/bootstrap/dist/js/thrustdrilling.js
@@ -14,5 +14,35 @@ $(function(){
         $('.columns').hide('slow');
         $('.column').hide('slow');
     });
+    $('#anime2').click(function(){
+        $('.col4').show('slow');
+    });
+
+    $('#anime3').click(function(){
+        $('.column').each(function(){
+            $( this ).show('slow');
+        });
+        $('.columns').each(function(){
+            $( this ).show('slow');
+        });
+        $('#col8').hide('slow');
+    });
+
+    $('#anime4').click(function(){
+        $('.column').each(function(){
+            $( this ).show('slow');
+        });
+        $('.columns').each(function(){
+            $( this ).show('slow');
+        });
+    });
+
+    $('#anime5').click(function(){
+        $('.columns').hide('slow');
+        $('.column').hide('slow');
+    });
+    $('#anime2').click(function(){
+        $('.col5').show('slow');
+    });
 
 });
