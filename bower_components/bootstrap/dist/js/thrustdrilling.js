9858fe05acdda34298fb1b3bc0f5b0f80287a2bd
Copser <iamrmot@gmail.com>
2016-04-26 22:05:32 +0200

    text manipulation"

--- bower_components/bootstrap/dist/js/thrustdrilling.js
+++ bower_components/bootstrap/dist/js/thrustdrilling.js
@@ -3,46 +3,69 @@ $(function(){
 
     $('#anime1').click(function(){
         $('.column').each(function(){
-            $( this ).hide('slow');
+            $( this ).hide( 500 );
         });
         $('.columns').each(function(){
-            $( this ).show('slow');
+            $( this ).show( 500 );
         });
+        $('#col1 p:first').text('Grouting text will be displayed here!');
+        $('#col2 p:first').text('Grouting text will be displayed here!');
+        $('#col3 p:first').text('Grouting text will be displayed here!');
+        $('#col4 p:first').text('Grouting text will be displayed here!');
+        $('#col5 p:first').text('Grouting text will be displayed here!');
+        $('#col6 p:first').text('Grouting text will be displayed here!');
     });
 
     $('#anime2').click(function(){
-        $('.columns').hide('slow');
-        $('.column').hide('slow');
+        $('.columns').hide( 500 );
+        $('.column').hide( 500 );
     });
     $('#anime2').click(function(){
-        $('.col4').show('slow');
+        $('.col4').show( 500 );
+        $('#col4 p:first').text('Ground Improvements text will be displayed here!');
     });
 
     $('#anime3').click(function(){
         $('.column').each(function(){
-            $( this ).show('slow');
+            $( this ).show( 500 );
         });
         $('.columns').each(function(){
-            $( this ).show('slow');
+            $( this ).show( 500 );
         });
-        $('#col8').hide('slow');
+        $('#col1 p:first').text('Earth Retention text will be displayed here!');
+        $('#col2 p:first').text('Earth Retention text will be displayed here!');
+        $('#col3 p:first').text('Earth Retention text will be displayed here!');
+        $('#col4 p:first').text('Earth Retention text will be displayed here!');
+        $('#col5 p:first').text('Earth Retention text will be displayed here!');
+        $('#col6 p:first').text('Earth Retention text will be displayed here!');
+        $('#col7 p:first').text('Earth Retention text will be displayed here!');
+        $('#col8').hide( 500 );
     });
 
     $('#anime4').click(function(){
         $('.column').each(function(){
-            $( this ).show('slow');
+            $( this ).show( 500 );
         });
         $('.columns').each(function(){
-            $( this ).show('slow');
+            $( this ).show( 500 );
         });
+        $('#col1 p:first').text('Structural Support text will be displayed here!');
+        $('#col2 p:first').text('Structural Support text will be displayed here!');
+        $('#col3 p:first').text('Structural Support text will be displayed here!');
+        $('#col4 p:first').text('Structural Support text will be displayed here!');
+        $('#col5 p:first').text('Structural Support text will be displayed here!');
+        $('#col6 p:first').text('Structural Support text will be displayed here!');
+        $('#col7 p:first').text('Structural Support text will be displayed here!');
+        $('#col8 p:first').text('Structural Support text will be displayed here!');
     });
 
     $('#anime5').click(function(){
-        $('.columns').hide('slow');
-        $('.column').hide('slow');
+        $('.columns').hide( 500 );
+        $('.column').hide( 500 );
     });
     $('#anime2').click(function(){
-        $('.col5').show('slow');
+        $('.col5').show( 500 );
+        $('#col5 p:first').text('Additional Technologies text will be displayed here!');
     });
 
 });
