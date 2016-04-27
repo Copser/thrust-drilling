d7c52dd04a7634c9757ea555ad0e5217c0cc099d
Copser <iamrmot@gmail.com>
2016-04-27 22:25:25 +0200

    adding h3 tag to code

--- bower_components/bootstrap/dist/js/thrustdrilling.js
+++ bower_components/bootstrap/dist/js/thrustdrilling.js
@@ -7,13 +7,25 @@ $(function(){
         });
         $('.columns').each(function(){
             $( this ).show( 500 );
+            
+        $('#col1 h3').text('Grouting!');
+        $('#col1 p:first').text('Grouting retention text will be displayed here!');
+
+        $('#col2 h3').text('Grouting!');
+        $('#col2 p:first').text('Grouting retention text will be displayed here!');
+
+        $('#col3 h3').text('Grouting!');
+        $('#col3 p:first').text('Grouting retention text will be displayed here!');
+
+        $('#col4 h3').text('Grouting!');
+        $('#col4 p:first').text('Grouting retention text will be displayed here!');
+
+        $('#col5 h3').text('Grouting!');
+        $('#col5 p:first').text('Grouting retention text will be displayed here!');
+        
+        $('#col6 h3').text('Grouting!');
+        $('#col6 p:first').text('Grouting retention text will be displayed here!');
         });
-        $('#col1 p:first').text('Grouting text will be displayed here!');
-        $('#col2 p:first').text('Grouting text will be displayed here!');
-        $('#col3 p:first').text('Grouting text will be displayed here!');
-        $('#col4 p:first').text('Grouting text will be displayed here!');
-        $('#col5 p:first').text('Grouting text will be displayed here!');
-        $('#col6 p:first').text('Grouting text will be displayed here!');
     });
 
     $('#anime2').click(function(){
@@ -22,6 +34,7 @@ $(function(){
     });
     $('#anime2').click(function(){
         $('.col4').show( 500 );
+        $('#col4 h3').text('Ground Improvements!');
         $('#col4 p:first').text('Ground Improvements text will be displayed here!');
     });
 
@@ -32,13 +45,26 @@ $(function(){
         $('.columns').each(function(){
             $( this ).show( 500 );
         });
-        $('#col1 p:first').text('Earth Retention text will be displayed here!');
-        $('#col2 p:first').text('Earth Retention text will be displayed here!');
-        $('#col3 p:first').text('Earth Retention text will be displayed here!');
-        $('#col4 p:first').text('Earth Retention text will be displayed here!');
-        $('#col5 p:first').text('Earth Retention text will be displayed here!');
-        $('#col6 p:first').text('Earth Retention text will be displayed here!');
-        $('#col7 p:first').text('Earth Retention text will be displayed here!');
+        $('#col1 h3').text('Earth Retention!');
+        $('#col1 p:first').text('Earth Retention retention text will be displayed here!');
+
+        $('#col2 h3').text('Earth Retention!');
+        $('#col2 p:first').text('Earth Retention retention text will be displayed here!');
+
+        $('#col3 h3').text('Earth Retention!');
+        $('#col3 p:first').text('Earth Retention retention text will be displayed here!');
+
+        $('#col4 h3').text('Earth Retention!');
+        $('#col4 p:first').text('Earth Retention retention text will be displayed here!');
+
+        $('#col5 h3').text('Earth Retention!');
+        $('#col5 p:first').text('Earth Retention retention text will be displayed here!');
+        
+        $('#col6 h3').text('Earth Retention!');
+        $('#col6 p:first').text('Earth Retention retention text will be displayed here!');
+
+        $('#col7 h3').text('Earth Retention!');
+        $('#col7 p:first').text('Earth Retention retention text will be displayed here!');
         $('#col8').hide( 500 );
     });
 
@@ -49,23 +75,40 @@ $(function(){
         $('.columns').each(function(){
             $( this ).show( 500 );
         });
+        $('#col1 h3').text('Structural Support!');
         $('#col1 p:first').text('Structural Support text will be displayed here!');
+
+        $('#col2 h3').text('Structural Support!');
         $('#col2 p:first').text('Structural Support text will be displayed here!');
+
+        $('#col3 h3').text('Structural Support!');
         $('#col3 p:first').text('Structural Support text will be displayed here!');
+
+        $('#col4 h3').text('Structural Support!');
         $('#col4 p:first').text('Structural Support text will be displayed here!');
+
+        $('#col5 h3').text('Structural Support!');
         $('#col5 p:first').text('Structural Support text will be displayed here!');
+
+        $('#col6 h3').text('Structural Support!');
         $('#col6 p:first').text('Structural Support text will be displayed here!');
+
+        $('#col7 h3').text('Structural Support!');
         $('#col7 p:first').text('Structural Support text will be displayed here!');
+
+        $('#col8 h3').text('Structural Support!');
         $('#col8 p:first').text('Structural Support text will be displayed here!');
+
     });
 
-    $('#anime5').click(function(){
-        $('.columns').hide( 500 );
-        $('.column').hide( 500 );
-    });
-    $('#anime2').click(function(){
-        $('.col5').show( 500 );
-        $('#col5 p:first').text('Additional Technologies text will be displayed here!');
-    });
+        $('#anime5').click(function(){
+            $('.columns').hide( 500 );
+            $('.column').hide( 500 );
+        });
+        $('#anime5').click(function(){
+            $('.col5').show( 500 );
+            $('#col5 h3').text('Additional!');
+            $('#col5 p:first').text('Additional Technologies text will be displayed here!');
+        });
 
 });
